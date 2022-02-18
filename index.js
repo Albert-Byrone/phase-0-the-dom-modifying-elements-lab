f72c4d3bcdba3520f837fd2d2fd16c0b4764618d
Albert Byrone <albertbyrone1677@gmail.com>
2022-02-18 12:54:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const main = document.getElementById("main");
+
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.innerHTML = "Albert is the champion"
+
+
+// const newHeader = document.createElement("h1#victory");
